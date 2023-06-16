e4a48ddb629bccfad5e9d377b4261b0d9a83b504
jpvargass <jpvargass@gmail.com>
2023-06-15 21:15:18 -0400

    Initial commit

---
+++ app/src/main/java/com/example/modulo4_ej3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.modulo4_ej3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
