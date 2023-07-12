b5322567bb18bcde4b19907d52e9886c8968ea5f
JpVargass <136398972+JpVargass@users.noreply.github.com>
2023-07-11 20:31:57 -0400

    Nueva version en kotlin del ejercicio 4 modulo 4 (java)

--- app/src/main/java/com/example/modulo4_ej3/MainActivity.kt
+++ app/src/main/java/com/example/modulo4_ej3/MainActivity.kt
@@ -1,88 +1,65 @@
-package com.example.modulo4_ej3;
+package com.example.modulo4_ej3
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button sumar = findViewById(R.id.sumar);
-        Button restar = findViewById(R.id.restar);
-        Button multiplicar = findViewById(R.id.multiplicar);
-        Button dividir = findViewById(R.id.dividir);
-
-        EditText editTextprimerNumero = findViewById(R.id.editTextPrimerNumero);
-        EditText editTextsegundoNumero = findViewById(R.id.editTextSegundoNumero);
-        sumar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(editTextprimerNumero.getText().toString().trim().isEmpty() || editTextsegundoNumero.getText().toString().trim().isEmpty()){    // validación
-                    Toast.makeText(getBaseContext(),  "(suma)faltan numeros a ingresar " , Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                 Integer primer = Integer.parseInt(editTextprimerNumero.getText().toString());
-                Integer segundo = Integer.parseInt(editTextsegundoNumero.getText().toString());
-                Integer suma = primer + segundo;
-                Toast.makeText(getBaseContext(),"resultado: "+ suma.toString(), Toast.LENGTH_LONG).show();
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val sumar = findViewById<Button>(R.id.sumar)
+        val restar = findViewById<Button>(R.id.restar)
+        val multiplicar = findViewById<Button>(R.id.multiplicar)
+        val dividir = findViewById<Button>(R.id.dividir)
+        val editTextprimerNumero = findViewById<EditText>(R.id.editTextPrimerNumero)
+        val editTextsegundoNumero = findViewById<EditText>(R.id.editTextSegundoNumero)
+        sumar.setOnClickListener(View.OnClickListener {
+            if (editTextprimerNumero.text.toString().trim { it <= ' ' }.isEmpty() || editTextsegundoNumero.text.toString().trim { it <= ' ' }.isEmpty()) {    // validación
+                Toast.makeText(baseContext, "(suma)faltan numeros a ingresar ", Toast.LENGTH_SHORT).show()
+                return@OnClickListener
             }
-
-        });
-        restar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(editTextprimerNumero.getText().toString().trim().isEmpty() || editTextsegundoNumero.getText().toString().trim().isEmpty()){    // validación
-                    Toast.makeText(getBaseContext(),  "(resta)faltan numeros a ingresar " , Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                Integer primer = Integer.parseInt(editTextprimerNumero.getText().toString());
-                Integer segundo = Integer.parseInt(editTextsegundoNumero.getText().toString());
-                Integer resta = primer - segundo;
-                Toast.makeText(getBaseContext(),"resultado: "+ resta.toString(), Toast.LENGTH_LONG).show();
+            val primer = editTextprimerNumero.text.toString().toInt()
+            val segundo = editTextsegundoNumero.text.toString().toInt()
+            val suma = primer + segundo
+            Toast.makeText(baseContext, "resultado: $suma", Toast.LENGTH_LONG).show()
+        })
+        restar.setOnClickListener(View.OnClickListener {
+            if (editTextprimerNumero.text.toString().trim { it <= ' ' }.isEmpty() || editTextsegundoNumero.text.toString().trim { it <= ' ' }.isEmpty()) {    // validación
+                Toast.makeText(baseContext, "(resta)faltan numeros a ingresar ", Toast.LENGTH_SHORT).show()
+                return@OnClickListener
             }
-
-        });
-        dividir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (editTextprimerNumero.getText().toString().trim().isEmpty() || editTextsegundoNumero.getText().toString().trim().isEmpty()){
-                    Toast.makeText(getBaseContext(),  "(division)faltan numeros a ingresar " , Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                Integer primer = Integer.parseInt(editTextprimerNumero.getText().toString());
-                if (Integer.parseInt(editTextsegundoNumero.getText().toString())==0) {
-                    Toast.makeText(getBaseContext(),  "no se puede dividir por 0 " , Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                Integer segundo = Integer.parseInt(editTextsegundoNumero.getText().toString());
-                Integer division = primer / segundo;
-                Toast.makeText(getBaseContext(),  "resultado :  "+division , Toast.LENGTH_SHORT).show();
-
-
-                }
-
-            });
-        multiplicar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(editTextprimerNumero.getText().toString().trim().isEmpty() || editTextsegundoNumero.getText().toString().trim().isEmpty()){    // validación
-                    Toast.makeText(getBaseContext(),  "(multiplicacion)faltan numeros a ingresar " , Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                Integer primer = Integer.parseInt(editTextprimerNumero.getText().toString());
-                Integer segundo = Integer.parseInt(editTextsegundoNumero.getText().toString());
-                Integer multiplicacion = primer * segundo;
-                Toast.makeText(getBaseContext(),"resultado: "+ multiplicacion.toString(), Toast.LENGTH_LONG).show();
+            val primer = editTextprimerNumero.text.toString().toInt()
+            val segundo = editTextsegundoNumero.text.toString().toInt()
+            val resta = primer - segundo
+            Toast.makeText(baseContext, "resultado: $resta", Toast.LENGTH_LONG).show()
+        })
+        dividir.setOnClickListener(View.OnClickListener {
+            if (editTextprimerNumero.text.toString().trim { it <= ' ' }.isEmpty() || editTextsegundoNumero.text.toString().trim { it <= ' ' }.isEmpty()) {
+                Toast.makeText(baseContext, "(division)faltan numeros a ingresar ", Toast.LENGTH_SHORT).show()
+                return@OnClickListener
             }
-
-        });
-        }
-
+            val primer = editTextprimerNumero.text.toString().toInt()
+            if (editTextsegundoNumero.text.toString().toInt() == 0) {
+                Toast.makeText(baseContext, "no se puede dividir por 0 ", Toast.LENGTH_SHORT).show()
+                return@OnClickListener
+            }
+            val segundo = editTextsegundoNumero.text.toString().toInt()
+            val division = primer / segundo
+            Toast.makeText(baseContext, "resultado :  $division", Toast.LENGTH_SHORT).show()
+        })
+        multiplicar.setOnClickListener(View.OnClickListener {
+            if (editTextprimerNumero.text.toString().trim { it <= ' ' }.isEmpty() || editTextsegundoNumero.text.toString().trim { it <= ' ' }.isEmpty()) {    // validación
+                Toast.makeText(baseContext, "(multiplicacion)faltan numeros a ingresar ", Toast.LENGTH_SHORT).show()
+                return@OnClickListener
+            }
+            val primer = editTextprimerNumero.text.toString().toInt()
+            val segundo = editTextsegundoNumero.text.toString().toInt()
+            val multiplicacion = primer * segundo
+            Toast.makeText(baseContext, "resultado: $multiplicacion", Toast.LENGTH_LONG).show()
+        })
     }
+}
